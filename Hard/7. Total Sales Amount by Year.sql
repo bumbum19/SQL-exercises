7035bbc1df12daf1b57f656007de0a134d5f1b1d
bumbum19 <64534242+bumbum19@users.noreply.github.com>
2022-11-14 14:17:05 +0100

    Update 7. Total Sales Amount by Year.sql

--- Hard/7. Total Sales Amount by Year.sql
+++ Hard/7. Total Sales Amount by Year.sql
@@ -72,10 +72,21 @@ Explanation:
 LC Phone was sold for the period of 2019-01-25 to 2019-02-28, and there are 35 days for this period. Total amount 35*100 = 3500. 
 LC T-shirt was sold for the period of 2018-12-01 to 2020-01-01, and there are 31, 365, 1 days for years 2018, 2019 and 2020 respectively.
 LC Keychain was sold for the period of 2019-12-01 to 2020-01-31, and there are 31, 31 days for years 2019 and 2020 respectively.
+*/
 
+# Solution 
 
+WITH t AS 
+(SELECT 2018 report_year UNION SELECT 2019 report_year UNION SELECT 2020 report_year
+)
 
-
-
-
- */
+SELECT product_id, product_name, CAST(report_year AS char) report_year ,SUM(CASE WHEN YEAR(period_start) = report_year AND  YEAR(period_end) = report_year
+THEN DATEDIFF(period_end,period_start)+1
+WHEN YEAR(period_start) = report_year THEN 365-DAYOFYEAR(period_start)+1
+WHEN YEAR(period_end) = report_year THEN DAYOFYEAR(period_end)
+ELSE IF(report_year=2019 AND YEAR(period_start)=2018
+AND YEAR(period_end)=2020,365,0) END *average_daily_sales ) total_amount
+FROM (t,sales) 
+NATURAL JOIN product
+GROUP BY 1,2,3
+HAVING total_amount > 0 ORDER BY 1,3
