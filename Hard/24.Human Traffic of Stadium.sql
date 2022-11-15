88e2630159524cbe7b46809ec534cd2991d6a838
bumbum19 <64534242+bumbum19@users.noreply.github.com>
2022-11-15 12:23:55 +0100

    Create 24.Human Traffic of Stadium.sql

---
+++ Hard/24.Human Traffic of Stadium.sql
@@ -0,0 +1,70 @@
+/* 
+Question: 601
+Table: Stadium
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| visit_date    | date    |
+| people        | int     |
++---------------+---------+
+visit_date is the primary key for this table.
+Each row of this table contains the visit date and visit id to the stadium with the number of people during the visit.
+No two rows will have the same visit_date, and as the id increases, the dates increase as well.
+
+ 
+
+Write an SQL query to display the records with three or more rows with consecutive id's, and the number of people is greater than or equal to 100 for each.
+
+Return the result table ordered by visit_date in ascending order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Stadium table:
++------+------------+-----------+
+| id   | visit_date | people    |
++------+------------+-----------+
+| 1    | 2017-01-01 | 10        |
+| 2    | 2017-01-02 | 109       |
+| 3    | 2017-01-03 | 150       |
+| 4    | 2017-01-04 | 99        |
+| 5    | 2017-01-05 | 145       |
+| 6    | 2017-01-06 | 1455      |
+| 7    | 2017-01-07 | 199       |
+| 8    | 2017-01-09 | 188       |
++------+------------+-----------+
+Output: 
++------+------------+-----------+
+| id   | visit_date | people    |
++------+------------+-----------+
+| 5    | 2017-01-05 | 145       |
+| 6    | 2017-01-06 | 1455      |
+| 7    | 2017-01-07 | 199       |
+| 8    | 2017-01-09 | 188       |
++------+------------+-----------+
+Explanation: 
+The four rows with ids 5, 6, 7, and 8 have consecutive ids and each of them has >= 100 people attended. Note that row 8 was included even though the visit_date was not the next day after row 7.
+The rows with ids 2 and 3 are not included because we need at least three consecutive ids.
+
+*/
+
+# Solution
+
+
+WITH table1 AS
+(SELECT id, visit_date, LAG(people,2) OVER w AS p_lag2, 
+LAG(people) OVER w AS p_lag, people, LEAD(people) OVER w AS p_lead,
+LEAD(people,2) OVER w AS p_lead2 FROM stadium  WINDOW w AS (ORDER BY id) )
+
+SELECT id, visit_date, people FROM table1 WHERE people >= 100 AND (
+    (p_lag2 >= 100 AND p_lag >= 100) OR
+    (p_lag >= 100 AND p_lead >= 100) OR
+    (p_lead >= 100 AND p_lead2 >= 100)  ) 
+   
+    
