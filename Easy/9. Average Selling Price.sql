53dd33487ec0238e7ba4452b81943c37a0f008a6
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-11-23 11:00:10 +0100

    Create 9. Average Selling Price.sql

---
+++ Easy/9. Average Selling Price.sql
@@ -0,0 +1,35 @@
+/* 
+Question: 1251
+Table: Prices
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| start_date    | date    |
+| end_date      | date    |
+| price         | int     |
++---------------+---------+
+(product_id, start_date, end_date) is the primary key for this table.
+Each row of this table indicates the price of the product_id in the period from start_date to end_date.
+For each product_id there will be no two overlapping periods. That means there will be no two intersecting periods for the same product_id.
+
+ 
+
+Table: UnitsSold
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| purchase_date | date    |
+| units         | int     |
++---------------+---------+
+There is no primary key for this table, it may contain duplicates.
+Each row of this table indicates the date, units, and product_id of each product sold. 
+ */
+ 
+ # Solution
+ 
+ SELECT product_id, ROUND(SUM(units*price)/SUM(units),2) AS average_price  FROM unitssold NATURAL  JOIN prices  
+ WHERE purchase_date BETWEEN start_date AND  end_date GROUP BY product_id
