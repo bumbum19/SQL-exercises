2a0561a08f0fde51ade6ef48557db254a34f66e8
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-11-23 09:39:53 +0100

    Update 5. All the Matches of the League.sql

--- Easy/5. All the Matches of the League.sql
+++ Easy/5. All the Matches of the League.sql
@@ -46,4 +46,8 @@ Explanation: All the matches of the league are shown in the table.
 
  */
  
+Solution:
+ 
+SELECT t1.team_name home_team, t2.team_name away_team FROM Teams t1 CROSS JOIN Teams t2
+WHERE t1.team_name <> t2.team_name
  
