4083ae301af9b7f87dd0f223c1fee4c5e873a1fd
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-11-24 05:21:36 +0100

    Create 23. onsecutive Available Seats.sql

---
+++ Easy/23. onsecutive Available Seats.sql
@@ -0,0 +1,58 @@
+/* 
+Question: 603
+Table: Cinema
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| seat_id     | int  |
+| free        | bool |
++-------------+------+
+seat_id is an auto-increment primary key column for this table.
+Each row of this table indicates whether the ith seat is free or not. 1 means free while 0 means occupied.
+
+ 
+
+Write an SQL query to report all the consecutive available seats in the cinema.
+
+Return the result table ordered by seat_id in ascending order.
+
+The test cases are generated so that more than two seats are consecutively available.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Cinema table:
++---------+------+
+| seat_id | free |
++---------+------+
+| 1       | 1    |
+| 2       | 0    |
+| 3       | 1    |
+| 4       | 1    |
+| 5       | 1    |
++---------+------+
+Output: 
++---------+
+| seat_id |
++---------+
+| 3       |
+| 4       |
+| 5       |
++---------+
+
+ */
+ 
+# Solution
+ 
+
+
+WITH table1 AS
+(SELECT seat_id, free, LEAD(free) OVER w AS forward, LAG(free) OVER w AS back
+FROM cinema WINDOW w AS (ORDER BY seat_id))
+
+SELECT seat_id FROM table1 WHERE free = 1 AND (forward = 1 OR back = 1) ORDER BY seat_id
