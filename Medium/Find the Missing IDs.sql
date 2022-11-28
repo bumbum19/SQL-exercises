f87fc26d88446e2595f63b6c7a0c5fed07ba4345
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-11-28 11:36:24 +0100

    Create Find the Missing IDs.sql

---
+++ Medium/Find the Missing IDs.sql
@@ -0,0 +1,59 @@
+/*
+Question: 1613
+Table: Customers
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| customer_id   | int     |
+| customer_name | varchar |
++---------------+---------+
+customer_id is the primary key for this table.
+Each row of this table contains the name and the id customer.
+
+ 
+
+Write an SQL query to find the missing customer IDs. The missing IDs are ones that are not in the Customers table but are in the range between 1 and the maximum customer_id present in the table.
+
+Notice that the maximum customer_id will not exceed 100.
+
+Return the result table ordered by ids in ascending order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customers table:
++-------------+---------------+
+| customer_id | customer_name |
++-------------+---------------+
+| 1           | Alice         |
+| 4           | Bob           |
+| 5           | Charlie       |
++-------------+---------------+
+Output: 
++-----+
+| ids |
++-----+
+| 2   |
+| 3   |
++-----+
+Explanation: 
+The maximum customer_id present in the table is 5, so in the range [1,5], IDs 2 and 3 are missing from the table.
+
+*/
+
+# Solution
+
+WITH RECURSIVE t (customer_id ) AS
+(
+  SELECT 1
+  UNION ALL
+  SELECT customer_id + 1 FROM t WHERE customer_id < (SELECT MAX(customer_id) FROM customers )
+)
+
+SELECT customer_id ids  FROM t NATURAL LEFT JOIN customers 
+WHERE customer_name IS NULL ORDER BY 1
