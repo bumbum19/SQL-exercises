465057b6ffaeccf70ca67fa030927eb956d7fec2
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-02 12:07:46 +0100

    Create Consecutive Numbers.sql

---
+++ Medium/Consecutive Numbers.sql
@@ -0,0 +1,54 @@
+/*
+Question: 180
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+id is the primary key for this table.
+id is an autoincrement column.
+
+ 
+
+Write an SQL query to find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+
+*/
+
+# Solution
+
+SELECT  DISTINCT num ConsecutiveNums  FROM
+(SELECT num, LAG(num) OVER w num_lag,LEAD(num) OVER w num_lead FROM logs
+ WINDOW w AS (ORDER BY id)) t
+ WHERE num=num_lag AND num_lag = num_lead 
