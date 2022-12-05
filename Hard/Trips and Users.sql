442df76e1bd03399fd696461efc2a1a6e3379b15
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-05 13:44:40 +0100

    Update and rename 10. Trips and Users.sql to Trips and Users.sql

--- Hard/Trips and Users.sql
+++ Hard/Trips and Users.sql
@@ -103,14 +103,14 @@ On 2013-10-03:
  
  # Solution
  
- WITH table1 AS
+WITH t AS
 (
-SELECT request_at AS Day, status FROM trips t JOIN users u ON t.client_id = u.users_id JOIN users u2 ON 
+SELECT request_at Day, status FROM trips t JOIN users u ON t.client_id = u.users_id JOIN users u2 ON 
 t.driver_id = u2.users_id WHERE u.banned = 'No' AND u2.banned = 'No' AND
 request_at BETWEEN '2013-10-01' AND '2013-10-03'
 )
 
 SELECT Day, 
-ROUND(SUM(CASE WHEN status='completed' THEN 0 ELSE 1 END)/COUNT(*),2)
+ROUND(AVG(CASE WHEN status='completed' THEN 0 ELSE 1 END),2)
 AS 'Cancellation Rate'
-FROM table1 GROUP BY Day
+FROM t GROUP BY 1
