84c8b30404d10ce80fc3fca6a94ccc23f094071f
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-07 13:23:19 +0100

    Update Group Sold Products By The  Date.sql

--- Easy/Group Sold Products By The  Date.sql
+++ Easy/Group Sold Products By The  Date.sql
@@ -55,6 +55,9 @@ For 2020-06-02, the Sold item is (Mask), we just return it.
  
 # Solution
  
-SELECT sell_date, COUNT(DISTINCT product) num_sold, 
-GROUP_CONCAT(DISTINCT product ORDER BY product)
-products FROM  activities GROUP BY 1 
+SELECT 
+ sell_date, 
+ COUNT(DISTINCT product) AS num_sold, 
+ GROUP_CONCAT(DISTINCT product  ORDER BY product) AS products
+FROM activities 
+GROUP BY sell_date; 
