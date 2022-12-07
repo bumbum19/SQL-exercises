c47c2ca734cd9513b16fb41998d18735c9c10a0e
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-07 11:40:57 +0100

    Update Recyclable and Low Fat Products.sql

--- Easy/Recyclable and Low Fat Products.sql
+++ Easy/Recyclable and Low Fat Products.sql
@@ -49,5 +49,9 @@ Explanation: Only products 1 and 3 are both low fat and recyclable.
 
 # Solution
 
-SELECT product_id FROM products WHERE low_fats = 'Y' AND recyclable = 'Y'
+SELECT 
+product_id 
+FROM products 
+WHERE low_fats = 'Y' 
+AND recyclable = 'Y';
 
