bff38ded57cf8c681cfd51258856e50687bb0a0d
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-08 13:54:32 +0100

    Update NPV Queries.sql

--- Easy/NPV Queries.sql
+++ Easy/NPV Queries.sql
@@ -84,8 +84,10 @@ The npv values of all other queries can be found in the NPV table.
  # Solution
  
  SELECT 
- id, 
- year, 
- IFNULL(npv,0) AS npv 
-FROM queries 
- NATURAL LEFT JOIN npv;
+ q.id, 
+ q.year, 
+ COALESCE(npv,0) AS npv 
+FROM queries q
+LEFT JOIN npv n
+ ON q.id = n.id
+ AND q.year = n.year;
