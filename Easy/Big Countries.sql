f754ca024cc136c9914aca15803a81e0aab03971
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-09 12:43:34 +0100

    Update Big Countries.sql

--- Easy/Big Countries.sql
+++ Easy/Big Countries.sql
@@ -54,4 +54,10 @@ Output:
 
 # Solution
 
-SELECT name, population, area FROM world WHERE area >= 3000000 OR population >= 25000000
+SELECT 
+name, 
+population, 
+area 
+FROM world
+WHERE area >= 3000000 
+OR population >= 25000000;
