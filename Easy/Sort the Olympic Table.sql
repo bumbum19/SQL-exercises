292deb87d4938097e7435cac05896f675e9f632a
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-09 10:10:36 +0100

    Update Sort the Olympic Table.sql

--- Easy/Sort the Olympic Table.sql
+++ Easy/Sort the Olympic Table.sql
@@ -59,5 +59,11 @@ Israel comes before Egypt because it has more bronze medals.
 
 # Solution
 
-SELECT country, gold_medals , silver_medals, bronze_medals FROM Olympic 
-ORDER BY 2 DESC, 3 DESC, 4 DESC, 1 
+SELECT 
+country, 
+gold_medals, 
+silver_medals, 
+bronze_medals 
+FROM Olympic 
+ORDER BY gold_medals DESC, silver_medals DESC, 
+    bronze_medals DESC, country;
