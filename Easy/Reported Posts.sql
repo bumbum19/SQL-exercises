00629fcfb9c2ee5ecf693c9dbc236e1353aeaeea
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-10 07:39:21 +0100

    Update Reported Posts.sql

--- Easy/Reported Posts.sql
+++ Easy/Reported Posts.sql
@@ -58,6 +58,10 @@ Explanation: Note that we only care about report reasons with non-zero number of
 
 # Solution
 
-SELECT extra report_reason, COUNT(DISTINCT post_id) AS report_count  FROM actions
-WHERE action_date = '2019-07-04' AND extra IS NOT NULL AND action='report'  
-GROUP BY 1 HAVING report_reason <> ''
+SELECT 
+extra AS report_reason, 
+COUNT(DISTINCT post_id) AS report_count 
+FROM actions
+WHERE action_date = '2019-07-04' 
+AND action='report'  
+GROUP BY extra;
