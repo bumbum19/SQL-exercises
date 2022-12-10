87a227fa84a2f0cb176cdc6603ee96c946987d09
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-10 12:06:56 +0100

    Update Game Play Analysis II.sql

--- Easy/Game Play Analysis II.sql
+++ Easy/Game Play Analysis II.sql
@@ -50,5 +50,8 @@ Output:
 
 # Solution
 
-SELECT  DISTINCT player_id, FIRST_VALUE(device_id)  OVER w device_id 
-FROM activity WINDOW w AS (PARTITION BY player_id ORDER BY event_date ) 
+SELECT  
+DISTINCT player_id,
+FIRST_VALUE(device_id)  
+  OVER (PARTITION BY player_id ORDER BY event_date ) AS device_id 
+FROM activity; 
