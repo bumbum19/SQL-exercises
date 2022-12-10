83e5c2480715d648910ebdc68ce701e77d3a454f
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-10 07:49:01 +0100

    Update Calculate Special Bonus.sql

--- Easy/Calculate Special Bonus.sql
+++ Easy/Calculate Special Bonus.sql
@@ -54,7 +54,24 @@ The rest of the employees get a 100% bonus.
 
 # Solution
 
-SELECT employee_id, 
-IF(employee_id % 2 = 1 AND name NOT LIKE 'm%', salary, 0) bonus
-FROM employees
-ORDER BY employee_id
+-- MySQL, MS SQL Server
+
+SELECT 
+employee_id, 
+CASE WHEN employee_id % 2 = 1 
+     AND SUBSTRING(name,1,1) != 'M' THEN  salary
+     ELSE 0 END AS bonus
+     FROM employees
+ORDER BY employee_id;
+
+
+-- Oracle
+
+SELECT 
+employee_id, 
+CASE WHEN MOD(employee_id,2) = 1 
+     AND SUBSTR(name,1,1) != 'M' THEN  salary
+     ELSE 0 END AS bonus
+     FROM employees
+ORDER BY employee_id;
+
