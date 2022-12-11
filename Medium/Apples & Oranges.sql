c91ea82e868f8b2877e829808588ab1dd1404621
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-11 11:32:46 +0100

    Update Apples & Oranges.sql

--- Medium/Apples & Oranges.sql
+++ Medium/Apples & Oranges.sql
@@ -61,7 +61,7 @@ Day 2020-05-04, 15 apples and 16 oranges were sold (Difference 15 - 16 = -1).
 
 SELECT 
 sale_date, 
-SUM(CASE WHEN fruit='apples'THEN sold_num 
+SUM(CASE WHEN fruit='apples' THEN sold_num 
     ELSE -sold_num END) AS diff 
 FROM sales 
 GROUP BY sale_date 
@@ -71,7 +71,7 @@ ORDER BY sale_date;
 
 SELECT 
 TO_CHAR(sale_date) AS sale_date, 
-SUM(CASE WHEN fruit='apples'THEN sold_num 
+SUM(CASE WHEN fruit='apples' THEN sold_num 
     ELSE -sold_num END) AS diff 
 FROM sales 
 GROUP BY sale_date 
