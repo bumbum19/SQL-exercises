92976ae18089b4feaf2924c9fc4473ea163c8f88
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-14 11:44:17 +0100

    Update Shortest Distance in a Plane.sql

--- Medium/Shortest Distance in a Plane.sql
+++ Medium/Shortest Distance in a Plane.sql
@@ -44,5 +44,9 @@ Explanation: The shortest distance is 1.00 from point (-1, -1) to (-1, 2).
 
 # Solution
 
-SELECT ROUND(MIN(SQRT(POWER(p1.x-p2.x,2) + POWER(p1.y-p2.y,2))),2) AS shortest 
-FROM Point2D p1 JOIN Point2D p2 ON  p1.x <> p2.x OR p1.y <> p2.y
+SELECT 
+ROUND(MIN(SQRT(POWER(p1.x-p2.x,2) + POWER(p1.y-p2.y,2))),2) AS shortest 
+FROM Point2D p1 
+JOIN Point2D p2 
+  ON p1.x != p2.x 
+  OR p1.y != p2.y;
