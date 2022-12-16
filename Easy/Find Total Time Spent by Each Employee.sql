c94137eee0102b1eed734919795f2b1f0dd8b3a4
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-16 10:39:57 +0100

    Update Find Total Time Spent by Each Employee.sql

--- Easy/Find Total Time Spent by Each Employee.sql
+++ Easy/Find Total Time Spent by Each Employee.sql
@@ -54,7 +54,7 @@ Employee 2 has two events: one on day 2020-11-28 with a total of (33 - 3) = 30, 
 
  */
  
-# Solution(MySQL, MS SQL Server)
+# Solution
  
 SELECT 
 event_day AS day,
@@ -64,13 +64,6 @@ FROM employees
 GROUP BY event_day, emp_id;
  
  
-# Solution2(Oracle)
 
-SELECT 
-TO_CHAR(event_day, 'YYYY-MM-DD') AS day,
-emp_id, 
-SUM(out_time-in_time) AS total_time 
-FROM employees 
-GROUP BY event_day, emp_id;
 
 
