f5c793efb701a25427aaf981cc8595230f9bee79
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-17 12:24:00 +0100

    Update Investments in 2016.sql

--- Medium/Investments in 2016.sql
+++ Medium/Investments in 2016.sql
@@ -60,6 +60,12 @@ So, the result is the sum of tiv_2016 of the first and last record, which is 45.
 
 # Solution
 
-SELECT ROUND(SUM(tiv_2016),2) tiv_2016 FROM insurance i
-WHERE tiv_2015 IN (SELECT tiv_2015 FROM insurance WHERE pid != i.pid ) 
-AND (lat,lon ) NOT IN (SELECT lat, lon FROM insurance WHERE pid != i.pid )
+SELECT
+ROUND(SUM(tiv_2016),2) AS tiv_2016 
+FROM insurance i
+WHERE tiv_2015 IN 
+    (SELECT tiv_2015 FROM insurance 
+    WHERE pid != i.pid ) 
+AND (lat,lon ) NOT IN 
+    (SELECT lat, lon FROM insurance 
+    WHERE pid != i.pid );
