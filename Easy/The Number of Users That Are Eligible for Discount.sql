a43801e259cc37c6848b86203587179f603a07f3
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-18 10:57:53 +0100

    Update The Number of Users That Are Eligible for Discount.sql

--- Easy/The Number of Users That Are Eligible for Discount.sql
+++ Easy/The Number of Users That Are Eligible for Discount.sql
@@ -55,8 +55,10 @@ CREATE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS 
 BEGIN
   
   RETURN (
-      # Write your MySQL query statement below.
-	  SELECT COUNT(DISTINCT user_id) user_cnt FROM purchases  WHERE amount >= minAmount AND 
-      time_stamp >= startDate AND time_stamp <= endDate
+      SELECT COUNT(DISTINCT user_id) AS user_cnt 
+      FROM purchases 
+      WHERE amount >= minAmount 
+      AND time_stamp >= startDate 
+      AND time_stamp <= endDate
   );  
 END
