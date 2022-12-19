142c5823363be0ff63ba848efaf189bed0b76ab7
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-19 13:17:47 +0100

    Update Products With Three or More Orders in Two Consecutive Years.sql

--- Medium/Products With Three or More Orders in Two Consecutive Years.sql
+++ Medium/Products With Three or More Orders in Two Consecutive Years.sql
@@ -52,8 +52,6 @@ Product 2 was ordered one time in 2022. We do not include it in the answer.
 
 # Solution
 
--- MySQL, MS SQL Server
-
 WITH cte AS
 (SELECT product_id, 
  YEAR(purchase_date) AS current_year, 
@@ -69,24 +67,4 @@ SELECT
 DISTINCT product_id 
 FROM cte 
 WHERE next_year - current_year = 1;
-
--- Oracle, MySQL
-
-
-WITH cte AS
-(SELECT product_id, 
- EXTRACT(YEAR FROM purchase_date) AS current_year, 
- lead(EXTRACT(YEAR FROM purchase_date)) OVER 
-    (PARTITION BY product_id 
-    ORDER BY EXTRACT(YEAR FROM purchase_date) ) AS next_year
-FROM orders 
-GROUP BY product_id, EXTRACT(YEAR FROM purchase_date) 
-HAVING COUNT(*) >=3
-)
-
-SELECT 
-DISTINCT product_id 
-FROM cte 
-WHERE next_year - current_year = 1;
-
 
