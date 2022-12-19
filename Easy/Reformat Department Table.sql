5bb4006fb840d5a5ee69c35082c4ddaf72fbda21
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-19 12:40:23 +0100

    Update Reformat Department Table.sql

--- Easy/Reformat Department Table.sql
+++ Easy/Reformat Department Table.sql
@@ -51,7 +51,6 @@ Note that the result table has 13 columns (1 for the department id + 12 for the 
  
  # Solution 
  
--- MySQL
 
 SELECT  
 id,
@@ -69,57 +68,4 @@ SUM(CASE WHEN month = "Nov" THEN revenue  END) AS Nov_Revenue,
 SUM(CASE WHEN month = "Dec" THEN revenue  END) AS Dec_Revenue
 FROM department 
 GROUP BY id;
-
--- MS SQL Server
-
-SELECT 
-id, 
-Jan AS Jan_Revenue,
-Feb AS Feb_Revenue, 
-Mar AS Mar_Revenue, 
-Apr AS Apr_Revenue,
-May AS May_Revenue, 
-Jun AS Jun_Revenue, 
-Jul AS Jul_Revenue, 
-Aug AS Aug_Revenue,  
-Sep AS Sep_Revenue, 
-Oct AS Oct_Revenue, 
-Nov AS Nov_Revenue,
-Dec AS Dec_Revenue
-FROM (
-  SELECT id ,revenue, month
-  FROM department) AS SourceTable  
-PIVOT( SUM(revenue) FOR month IN ( Jan,Feb, Mar, Apr, May, Jun, 
-                                   Jul, Aug, Sep, Oct, Nov, Dec)  
-                                   ) AS pvt;
-
-
--- Oracle
-
-SELECT *
-FROM department
-PIVOT (
-  SUM(revenue)
-  FOR month
-  IN ('Jan' AS Jan_Revenue, 
-      'Feb' AS Feb_Revenue, 
-      'Mar' AS Mar_Revenue,
-      'Apr' AS Apr_Revenue,
-      'May' AS May_Revenue,
-      'Jun' AS Jun_Revenue,
-      'Jul' AS Jul_Revenue,
-      'Aug' AS Aug_Revenue,
-      'Sep' AS Sep_Revenue,
-      'Oct' AS Oct_Revenue,
-      'Nov' AS Nov_Revenue,
-      'Dec' AS Dec_Revenue
-       )
-);
-                                  
-      
-
-
-
-
-
 
