a226647468136cfd6debace9794b12de499b9025
fjunginger <64534242+bumbum19@users.noreply.github.com>
2022-12-19 13:15:52 +0100

    Update Monthly Transactions I.sql

--- Medium/Monthly Transactions I.sql
+++ Medium/Monthly Transactions I.sql
@@ -50,8 +50,6 @@ Output:
 
 # Solution
 
--- MySQL
-
 SELECT 
 DATE_FORMAT(trans_date, '%Y-%m') AS month, 
 country, 
@@ -62,29 +60,4 @@ SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM transactions 
 GROUP BY month, country
 
-
--- MS SQL Server
-
-SELECT 
-FORMAT(trans_date, 'yyyy-MM') AS month, 
-country, 
-COUNT(*) AS trans_count, 
-SUM(CASE WHEN state='approved' THEN 1 ELSE 0 END) AS approved_count, 
-SUM(amount) AS trans_total_amount,
-SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) AS approved_total_amount 
-FROM transactions 
-GROUP BY FORMAT(trans_date, 'yyyy-MM'), country;
-
-
--- Oracle
-
-SELECT 
-TO_CHAR(trans_date, 'YYYY-MM') AS month, 
-country, 
-COUNT(*) AS trans_count, 
-SUM(CASE WHEN state='approved' THEN 1 ELSE 0 END) AS approved_count, 
-SUM(amount) AS trans_total_amount,
-SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) AS approved_total_amount 
-FROM transactions 
-GROUP BY TO_CHAR(trans_date, 'YYYY-MM'), country;
 
